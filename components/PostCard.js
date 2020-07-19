350b2bf8b611512b823ab412c9f8d30ccb57bf3c
Karmic <karmiccreativedev@gmail.com>
2020-07-19 15:31:20 -0400

    🚧 Removed blog images from homepage

--- components/PostCard.js
+++ components/PostCard.js
@@ -13,9 +13,6 @@ export default function PostCard({
     <Link as={`/blog/${slug}`} href="/blog/[slug]">
       <a><div className="post-card">
         <p className="category">{category}</p>
-        <div className="post-image">
-          <img src={imageUrlFor(imageUrl).width(600).height(360).fit('crop').url()} alt={`Cover Image for ${title}`} />
-        </div>
         <h2 className="post-title">{title}</h2>
         <p className="excerpt">{excerpt}</p>
         <span className="date">{date}</span>
