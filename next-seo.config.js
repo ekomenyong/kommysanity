027286a2061c2eccd01102a4757c83d4e24d124b
Karmic <karmiccreativedev@gmail.com>
2020-07-23 12:08:54 -0400

    🎨 Changed OG Image for site

--- next-seo.config.js
+++ next-seo.config.js
@@ -9,9 +9,9 @@ export default {
     description: 'I\'m Ekom Enyong, an SEO professional of 8 years, a web designer, and frontend developer. Get in contact with me or read some of my thoughts.',
     images: [
       {
-        url: 'https://res.cloudinary.com/ekomenyong/image/upload/v1594851857/ekom-headshot_xz0qp3.jpg',
-        width: 1200,
-        height: 630,
+        url: 'https://res.cloudinary.com/ekomenyong/image/upload/v1595520439/ekomenyong-ogimage_wuc6xd.png',
+        width: 1000,
+        height: 600,
         alt: 'Headshot of Ekom Enyong'
       }
     ],
