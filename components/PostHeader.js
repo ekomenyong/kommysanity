11ace8848ed95e00e861ee0cadbfe986234f6422
Karmic <karmiccreativedev@gmail.com>
2020-07-31 16:48:17 -0400

    🎨 single post styling fixes

--- components/PostHeader.js
+++ components/PostHeader.js
@@ -6,16 +6,16 @@ import PostTitle from './PostTitle';
 export default function PostHeader({ title, subtitle, date, author, category, imageUrl }) {
   return (
     <div className="post-header container">
-      <div className="post-info">
-        <Author author={author.name} image={author.image} />
-        <div className="date sp">{date}</div>
-
-      </div>
-      <p className="post-category">{category}</p>
       <div className="post-title-box">
         <PostTitle>{title}</PostTitle>
         <p className="post-subtitle">{subtitle}</p>
       </div>
+      <div className="post-info">
+        <Author author={author.name} image={author.image} />
+        <div className="date sp">{date}</div>
+      </div>
+      <p className="post-category">{category}</p>
+
       <div className="single-post-image">
         <img
           src={imageUrlFor(imageUrl).width(1000).height(600).fit('crop').url()}
