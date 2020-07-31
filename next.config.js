54c27a0af9caa70eeca7cdcd068dccf6936e08ef
Karmic <karmiccreativedev@gmail.com>
2020-07-31 16:21:56 -0400

    🏗️ next9.5 rewrite fixes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@ module.exports = {
   env: {
     SANITY_DATASET_NAME: process.env.SANITY_DATASET_NAME,
     SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID
+  },
+  async rewrites() {
+    return [
+      // check if Next.js project routes match before we attempt proxying
+      {
+        source: '/sitemap.xml',
+        destination: '/_next/public/sitemap.xml'
+      },
+      {
+        source: '/feed.json',
+        destination: `/_next/public/feed.json`
+      }
+    ];
   }
 };
 
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -81,7 +81,7 @@ export async function getStaticProps({ params, preview = false }) {
       preview,
       post: data
     },
-    unstable_revalidate: 1
+    revalidate: 1
   };
 }
 
