f4d63cb7a493e3e275b03496b9235193ede8d0b7
Ekom Enyong <hello@ekomenyong.com>
2021-02-07 09:52:25 -0500

    Update to custom Plausible domain

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
           <link rel="manifest" href="/img/site.webmanifest" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-          <script async defer data-domain="ekomenyong.com" src="https://plausible.io/js/plausible.js"></script>
+          <script async defer data-domain="ekomenyong.com" src="https://stats.ekomenyong.com/js/index.js"></script>
         </Head>
         <body>
           <Main />
